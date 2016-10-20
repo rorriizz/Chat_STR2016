7d6413b8e095f4dc3c3601c9b1e9c5a48d596bbb
rorriizz Gold <rorriizz@gmail.com>
2016-10-20 10:31:53 -0600

    New changes
    send and receive msg

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/assets'));
+//Routes
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/chat.html');
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/chat.html');
+});
+
+//Socket
+io.on('connection', function(socket){
+  console.log('A user connected');
+  socket.on('disconnect', function(){
+    console.log('User desconnected');
+  });
+  socket.on('sending', function(msg){
+    //console.log('Message: ' + msg);
+    //io.emit('sending', msg); 
+    socket.broadcast.emit('sending', msg);
+  });
 });
 
 http.listen(3000, function(){
-	console.log('Listening on *:3000');
+  console.log('Listening on *:3000');
 });
